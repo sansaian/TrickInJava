d2363e3d87e367e99eaead4d578672e1855e127a
Max Shalavin <shalavinms@gmail.com>
2016-11-26 17:23:43 +0300

    initial commit for Generics,covariant,wild cards

---
+++ covariantGenericsWildCard/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Max Shalavin on 23.11.2016.
+ */
+
+/**
+ * Пример ковариации в массиве
+ */
+public class Main {
+    public static void main(String[] args) {
+        Cat [] cats = new Cat[2];
+        Animal[] animals;
+        animals = cats;
+        Dog dog1 = new Dog();
+        animals[0] = dog1;// ошибка при попытки выполнения
+        /**
+         * Так как наш массив является массивом обьекта Cat
+         */
+
+
+
+}
+}
